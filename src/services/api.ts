f8ff599ca67376fa52c1015f493b683960213804
Andrea Agostini <andrea@twcapps.com>
2020-01-03 11:05:44 +0100

    solve CORS proxy issue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,13 @@
 import axios, { AxiosInstance } from "axios"
 
 const API_KEY = "659d5c6b8f3d2447f090119e48202fdb"
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 /*************************************************/
 /* BASE SETUP */
 /*************************************************/
 const baseRequest: AxiosInstance = axios.create({
-  baseURL: `https://sandbox-api.brewerydb.com/v2`
+  baseURL: `${CORS_PROXY}https://sandbox-api.brewerydb.com/v2`
 });
 
 /*************************************************/
